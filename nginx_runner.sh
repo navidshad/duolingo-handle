5f8483861f5859cdaa92f9f18b916c272568d89d
Navid Shad <navidshad72@gmail.com>
2023-10-14 21:29:46 +0300

    Add nginx confic and CI action

---
+++ nginx_runner.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script is used to run nginx in a docker container
+
+$NGINX_CONTAINER_NAME = "nginx"
+$IS_NGINX_RUNNING = $(docker ps -a | grep $NGINX_CONTAINER_NAME)
+
+# stop and remove the container if it is already running
+if [ -n "$IS_NGINX_RUNNING" ]; then
+	docker stop $NGINX_CONTAINER_NAME
+	docker rm $NGINX_CONTAINER_NAME
+fi
+
+# run the container
+# Path: nginx.conf
+docker run -d -p 80:80 --name $NGINX_CONTAINER_NAME -v /nginx.conf:/etc/nginx/nginx.conf:ro nginx
+
+echo "nginx is running on port 80 in a docker container"

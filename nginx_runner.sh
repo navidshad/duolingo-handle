dafa0d468e67c55bbb90b08f750b5d9aecf6a700
Navid Shad <navidshad72@gmail.com>
2023-10-14 22:20:10 +0300

    tweak nginx ci

--- nginx_runner.sh
+++ nginx_runner.sh
@@ -9,4 +9,10 @@ docker rm $NGINX_CONTAINER_NAME >/dev/null
 
 # run the container
 # Path: nginx.conf
-docker run -d -p 80:80 --name $NGINX_CONTAINER_NAME -v /nginx.conf:/etc/nginx/nginx.conf:ro nginx
+docker run -d -p 80:80 --name $NGINX_CONTAINER_NAME -v /nginx:/etc/nginx:ro nginx
+
+# copy the nginx.conf file to the container
+# docker cp nginx.conf $NGINX_CONTAINER_NAME:/etc/nginx/nginx.conf
+
+# reload the nginx configuration
+# docker exec $NGINX_CONTAINER_NAME nginx -s reload

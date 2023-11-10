82693dd8130745374519ed2918be325f7d606de6
Navid Shad <navidshad72@gmail.com>
2023-11-10 20:35:52 +0300

    Add product website

---
+++ product_website/src/router/index.ts
@@ -0,0 +1,20 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/requirements',
+      name: 'requirements',
+      component: () => import('../views/RequirementsView.vue')
+    }
+  ]
+})
+
+export default router
